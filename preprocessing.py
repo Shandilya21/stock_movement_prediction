6a4741483ddcabce329f7f7d5a07c5fab6bc195c
Arunav Shandilya <arunavshandilya96@gmail.com>
2019-06-10 11:10:27 +0530

    Preprocess_data

---
+++ preprocessing.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+def new_dataset(dataset, step_size):
+	data_X, data_Y = [], []
+	for i in range(len(dataset)-step_size-1):
+		a = dataset[i:(i+step_size), 0]
+		data_X.append(a)
+		data_Y.append(dataset[i + step_size, 0])
+	return np.array(data_X), np.array(data_Y)
